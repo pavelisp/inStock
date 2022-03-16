d2852d391bda035e90dc6f84b3e279bcff98961d
Bryan Kwok <bryanpykwok@gmail.com>
2022-03-16 10:08:38 -0400

    pull origin data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import WareHouseDetails from "./components/WareHouseDetails/WareHouseDetails";
 import "./App.scss";
 import { Component } from "react";
 import { Route,Redirect,Switch,BrowserRouter } from "react-router-dom";
+import WarehouseList from './components/WarehouseList/WarehouseList';
 
 class App extends Component {
   render(){
@@ -12,7 +13,7 @@ class App extends Component {
       <Header/>
         <Switch>
           <Route path="/" exact to="" />
-          <Route path="/warehouse" component={""} />
+          <Route path="/warehouse" component={WarehouseList} />
           <Route path="/inventory" component={InventoryList} />
           {/* Routes are flexible right now, add or change as needed */}
         </Switch>
